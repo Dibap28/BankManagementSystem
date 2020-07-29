be20b9329e1a6e2bf1bdc2cbe9c22e5bd990e4fe
Dibap <dibap28@gmail.com>
2020-07-29 21:32:32 +0530

    removed warning messages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Routes from "./Routes";
-
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
